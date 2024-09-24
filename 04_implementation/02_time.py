f0e9f65c575185086336948468b988dbf2cde949
Oh-Seokjin <5hseokjin@gmail.com>
2024-09-24 20:57:06 +0900

    Create 02_time.py

---
+++ 04_implementation/02_time.py
@@ -0,0 +1,12 @@
+n = 5
+#####
+
+cnt = 0
+
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h)+str(m)+str(s):
+                cnt += 1
+
+print(cnt)
